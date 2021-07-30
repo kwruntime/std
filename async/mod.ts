1d579538be5717dea8e29b09c463f7b4381eb8c1
James <xtreme.maicolxt94@gmail.com>
2021-07-30 17:59:50 +0000

    async mod update

--- async/mod.ts
+++ async/mod.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
-export * from "./debounce.ts";
+export * from "./debounced.ts";
 export * from "./deferred.ts";
 export * from "./delay.ts";
 export * from "./mux_async_iterator.ts";
