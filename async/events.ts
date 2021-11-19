6b3942f0f8fe6f228e3cc0afa6c6717b7fbaa515
Ubuntu <ubuntu@machine1.lxd>
2021-11-19 05:29:16 +0000

    Add getEnumerator to asynceventemiiter

--- async/events.ts
+++ async/events.ts
@@ -3,7 +3,11 @@ import {EventIterator} from "./event_iterator.ts"
 
 export class AsyncEventEmitter extends EventEmitter{
 
-    
+        
+    getEnumerator(event: string | string[]){
+        return this.getIterator(event).enumerator
+    }
+
     getIterator(event: string | string[]){
         if(typeof event == "string"){
             event = [event]
