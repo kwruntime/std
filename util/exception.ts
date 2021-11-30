0afb8bb54b8fe84a3a06ba96385ec446d28a5b8a
James <xtreme.maicolxt94@gmail.com>
2021-11-30 02:36:21 +0000

    Fix Exception types

--- util/exception.ts
+++ util/exception.ts
@@ -1,34 +1,35 @@
 
 export class Exception extends Error {
-	code: string 
-	static create(message, innerException = null) {
-		var e;
-		e = new Exception(message);
+	code: string
+	innerException: Error
+	
+	static create(message:string, innerException = null) {
+		let e = new Exception(message)
 		if (innerException) {
-			e.innerException = innerException;
+			e.innerException = innerException
 		}
-		return e;
+		return e
 	}
 
-	putCode(code) {
-		this.code = code;
-		return this;
+	putCode(code: string) {
+		this.code = code
+		return this
 	}
 
-	putStack(stack) {
-		this.stack = stack;
-		return this;
+	putStack(stack: string) {
+		this.stack = stack
+		return this
 	}
 
-	putMessage(message) {
-		this.message = message;
-		return this;
+	putMessage(message: string) {
+		this.message = message
+		return this
 	}
 
 	raise() {
-		throw this;
+		throw this
 	}
 
-};
+}
 
-export default Exception;
+export default Exception
