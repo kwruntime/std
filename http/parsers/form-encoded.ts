4fe87b16ba635a9d0e3a458ac1c48e2a90b20c16
Ubuntu <ubuntu@desk.lxd>
2022-11-15 16:15:45 -0500

    Fix formencoded parser

--- http/parsers/form-encoded.ts
+++ http/parsers/form-encoded.ts
@@ -3,7 +3,7 @@ import qs from 'npm://qs@6.10.3'
 
 export class FormEncoded{
 
-	async parse(req: Request){
+	static async parse(req: Request){
 		let text = await req.body.text()
 		return qs.parse(text)
 	}
